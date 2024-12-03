e330245c95a6e3643a8958226c537c080305cd6e
Zhang Zhimin <p3.1415@qq.com>
2024-12-03 15:26:02 +0800

    save

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -4,15 +4,18 @@ set -e
 build_configure=$1
 shift 1
 
-CXX=clang++ CC=clang cmake . \
+cmake . \
+-G Ninja \
 -B build_${build_configure} \
 -DCMAKE_BUILD_TYPE=${build_configure} \
 -DCMAKE_INSTALL_PREFIX=build_${build_configure}/install \
 -DBOOST_INCLUDE_LIBRARIES="algorithm;variant;optional;fusion;spirit;multiprecision;process;dll" \
 -DLLVM_INCLUDE_TESTS=OFF \
+-DLLVM_ENABLE_DUMP=ON \
 -DLLVM_INCLUDE_BENCHMARKS=OFF \
 -DLLVM_OPTIMIZED_TABLEGEN=ON \
 -DLLVM_ENABLE_PROJECTS="compiler-rt" \
--DLLVM_ENABLE_ABI_BREAKING_CHECKS=OFF -DLLVM_ENABLE_RTTI=ON -DBUILD_SHARED_LIBS=OFF \
--G Ninja \
+-DLLVM_ENABLE_ABI_BREAKING_CHECKS=OFF \
+-DLLVM_ENABLE_RTTI=ON \
+-DBUILD_SHARED_LIBS=OFF \
 $@
