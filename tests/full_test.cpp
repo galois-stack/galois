52185d52bdaeba305d196b4d7b1c6f4a54e4a945
Zhang Zhimin <p3.1415@qq.com>
2025-03-05 13:20:33 +0800

    Remove useless graph

--- tests/full_test.cpp
+++ tests/full_test.cpp
@@ -1,10 +1,7 @@
 #include "galois_test.hpp"
 
 inline auto Full(Eigen::VectorXi64 shape, float value) {
-    auto ir_ts_type = f32(199);
-    auto ir_full_op = op::FullCreator::Create(ir_ts_type);
-    auto ir_full = graph::ComputeNode::Create(ir_full_op, {});
-    return ir_full;
+    // TODO
 }
 
 TEST(GaloisTests, TestFull) {}
