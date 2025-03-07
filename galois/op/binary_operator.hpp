0d70cf1b177a7d19b73b1cb6d846cbe9eb26336f
Zhang Zhimin <p3.1415@qq.com>
2025-03-08 01:28:29 +0800

    Add ArithmeticCreator

--- galois/op/binary_operator.hpp
+++ galois/op/binary_operator.hpp
@@ -24,9 +24,6 @@ class BinaryOperatorCreator : public OperatorCreator {
 
     void AffineExpress(std::vector<std::shared_ptr<ir::Tensor>> ir_inputs,
                        std::shared_ptr<ir::Builder> ir_builder) override {
-        // auto ir_output = ir_builder->Create<ir::Alloca>(
-        //     this->InferTypeImpl(ir_inputs[0]->type, ir_inputs[1]->type));
-        // set_zero_creator->AffineExpress({ir_output}, ir_builder);
         auto ir_output_type = this->InferTypeImpl(ir_inputs[0]->type, ir_inputs[1]->type);
         auto ir_output = ir_builder->Express<op::FullCreator>({}, ir_output_type);
         this->AffineExpressImpl(ir_inputs[0], ir_inputs[1], ir_output, ir_builder);
--- galois/op/op.hpp
+++ galois/op/op.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "galois/op/add.hpp"
+#include "galois/op/arithmetic_operator.hpp"
 #include "galois/op/binary_operator.hpp"
 #include "galois/op/full.hpp"
 #include "galois/op/matrix_multiply_op.hpp"
