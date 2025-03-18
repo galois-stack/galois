213ae6dd26bb2b1a6893c484dd39d2512a03adb5
sunteng <steng2009@163.com>
2025-03-19 00:13:12 +0800

    Add sum full slice unary_intrinsic op benchmark tests (#11)

--- galois/op/slice.hpp
+++ galois/op/slice.hpp
@@ -1,11 +1,11 @@
 #pragma once
-
+#include "galois/ir/builder.hpp"
 #include "galois/ir/ir.hpp"
 #include "galois/op/creator.hpp"
 
 namespace galois::op {
 
-class SliceCreator : public Creator {
+class SliceCreator : public op::Creator {
    public:
     static std::shared_ptr<SliceCreator> Create(Eigen::VectorXi64 slice_shape) {
         std::shared_ptr<SliceCreator> self(new SliceCreator);
@@ -15,15 +15,15 @@ class SliceCreator : public Creator {
         return self;
     }
 
-    std::shared_ptr<TensorType> InferType(
-        std::vector<std::shared_ptr<TensorType>> ir_input_types) override {
+    std::shared_ptr<ir::TensorType> InferType(
+        std::vector<std::shared_ptr<ir::TensorType>> ir_input_types) override {
         GALOIS_ASSERT(ir_input_types.size() == 1);
-        return TensorType::Create(Cast<TensorType>(ir_input_types.front())->value_type,
+        return ir::TensorType::Create(Cast<ir::TensorType>(ir_input_types.front())->value_type,
                                   slice_shape);
     };
 
     void AffineExpress(std::vector<std::shared_ptr<ir::Tensor>> ir_inputs,
-                       std::shared_ptr<Builder> ir_builder) override {
+                       std::shared_ptr<ir::Builder> ir_builder) override {
         auto ir_output_type = this->InferType(ir::GetTensorTypes(ir_inputs));
         auto ir_output = ir_builder->Create<ir::Alloca>(ir_output_type);
         this->_Express({ir_inputs[0], ir_output}, ir_builder);
@@ -31,7 +31,7 @@ class SliceCreator : public Creator {
     }
 
     void _Express(std::vector<std::shared_ptr<ir::Tensor>> ir_inputs,
-                  std::shared_ptr<Builder> ir_builder) {
+                  std::shared_ptr<ir::Builder> ir_builder) {
         auto ir_input = ir_inputs[0];
         auto ir_output = ir_inputs[1];
         auto input_shape = ir_input->type->shape;
