012022f620093fef4d52c9812e27d623392f2ece
Zhang Zhimin <p3.1415@qq.com>
2025-03-22 16:42:50 +0800

    Add MatrixMultiplyTilePolicy

--- tests/gemm_test.cpp
+++ tests/gemm_test.cpp
@@ -4,24 +4,26 @@
 #include "galois/optimization/gemm_optimizer.hpp"
 #include "tests/galois_test.hpp"
 
-TEST(GaloisTests, TestPackedMatrixMultiply_F32x4x1x4) {
-    //  一种快捷写法, 需要用TensorTypePointer包装后才支持这种写法
-    auto ir_ts_type_a = ir::f32->Tile(4, 1)->Tile(2, 1)->Tile(1, 1024)->Tile(64, 1);
-    auto ir_ts_type_b = ir::f32->Tile(1, 4)->Tile(1, 3)->Tile(1024, 1)->Tile(1, 64);
-    ir_ts_type_a->value_type->enable_multi_thread = true;
+TEST(GaloisTests, TestPackedMatrixMultiply) {
+    auto native_cpu_info = optimization::NativeCpuInfo::Create();
+    auto matrix_tile_policy = optimization::MatrixMultiplyTilePolicy::Create();
+    auto [ir_mat_type_a, ir_mat_type_b] = matrix_tile_policy->Tile(ir::f32, native_cpu_info);
+    // 太小测不出性能， 所以又Tile了一次
+    ir_mat_type_a = ir_mat_type_a->Tile(64, 64);
+    ir_mat_type_b = ir_mat_type_b->Tile(64, 64);
 
     auto ir_builder = ir::Builder::Create();
     ir_builder->matrix_multiply_kernel_queue.push_back(
-        op::VectorizedMatrixMultiplyKernel::Create(128));
+        op::VectorizedMatrixMultiplyKernel::Create(native_cpu_info->SimdBits()));
     auto ir_packed_matrix_multiply_op_creator = op::MatrixMultiplyCreator::Create();
     auto ir_operator = ir_builder->CreateOperatorByCreator(ir_packed_matrix_multiply_op_creator,
-                                                           {ir_ts_type_a, ir_ts_type_b});
+                                                           {ir_mat_type_a, ir_mat_type_b});
 
     auto jit_engine = jit::Engine::Create();
     auto mat_mul_fun = jit_engine->EmitOperatorSymbol<float *(*)(float *, float *)>(ir_operator);
 
-    auto shape_a = ir_ts_type_a->NormalizeShape();
-    auto shape_b = ir_ts_type_b->NormalizeShape();
+    auto shape_a = ir_mat_type_a->NormalizeShape();
+    auto shape_b = ir_mat_type_b->NormalizeShape();
 
     Eigen::MatrixRXf32 eigen_matrix_f32_a = Eigen::MatrixRXf32::Ones(shape_a[0], shape_a[1]);
     Eigen::MatrixRXf32 eigen_matrix_f32_b = Eigen::MatrixRXf32::Ones(shape_b[0], shape_b[1]);
@@ -31,38 +33,6 @@ TEST(GaloisTests, TestPackedMatrixMultiply_F32x4x1x4) {
     auto t1 = std::chrono::high_resolution_clock::now();
     fmt::print("cost time: {}ns, galois flops: {:.04f}gops\n", (t1 - t0).count(),
                shape_a[0] * shape_a[1] * shape_b[1] * 2 / static_cast<double>((t1 - t0).count()));
-
-    free(ir_mat_c_ptr);
-}
-
-TEST(GaloisTests, TestPackedMatrixMultiply_F32x8x1x8) {
-    //  一种快捷写法, 需要用TensorTypePointer包装后才支持这种写法
-    auto ir_ts_type_a = ir::f32->Tile(8, 1)->Tile(2, 1)->Tile(1, 1024)->Tile(64, 1);
-    auto ir_ts_type_b = ir::f32->Tile(1, 8)->Tile(1, 1)->Tile(1024, 1)->Tile(1, 64);
-    ir_ts_type_a->value_type->enable_multi_thread = true;
-
-    auto ir_builder = ir::Builder::Create();
-    ir_builder->matrix_multiply_kernel_queue.push_back(
-        op::VectorizedMatrixMultiplyKernel::Create(256));
-    auto ir_packed_matrix_multiply_op_creator = op::MatrixMultiplyCreator::Create();
-    auto ir_operator = ir_builder->CreateOperatorByCreator(ir_packed_matrix_multiply_op_creator,
-                                                           {ir_ts_type_a, ir_ts_type_b});
-
-    auto jit_engine = jit::Engine::Create();
-    auto mat_mul_fun = jit_engine->EmitOperatorSymbol<float *(*)(float *, float *)>(ir_operator);
-
-    auto shape_a = ir_ts_type_a->NormalizeShape();
-    auto shape_b = ir_ts_type_b->NormalizeShape();
-
-    Eigen::MatrixRXf32 eigen_matrix_f32_a = Eigen::MatrixRXf32::Ones(shape_a[0], shape_a[1]);
-    Eigen::MatrixRXf32 eigen_matrix_f32_b = Eigen::MatrixRXf32::Ones(shape_b[0], shape_b[1]);
-
-    auto t0 = std::chrono::high_resolution_clock::now();
-    auto ir_mat_c_ptr = mat_mul_fun(eigen_matrix_f32_a.data(), eigen_matrix_f32_b.data());
-    auto t1 = std::chrono::high_resolution_clock::now();
-    fmt::print("cost time: {}ns, galois flops: {:.04f}gops\n", (t1 - t0).count(),
-               shape_a[0] * shape_a[1] * shape_b[1] * 2 / static_cast<double>((t1 - t0).count()));
-
     free(ir_mat_c_ptr);
 }
 
@@ -99,17 +69,17 @@ TEST(GaloisTests, TestPackedMatrixMultiply_i8x16x1x16) {
 
 TEST(GaloisTests, TestGemm) {
     //  一种快捷写法, 需要用TensorTypePointer包装后才支持这种写法
-    auto ir_ts_type_a = f32->Tile(1536 , 1024);
-    auto ir_ts_type_b = f32->Tile(1024 , 1024);
+    auto ir_mat_type_a = f32->Tile(1536, 1024);
+    auto ir_mat_type_b = f32->Tile(1024, 1024);
 
     auto ir_builder = ir::Builder::Create();
     auto ir_packed_matrix_multiply_op_creator = op::MatrixMultiplyCreator::Create();
-    auto ir_ts_type_c =
-        ir_packed_matrix_multiply_op_creator->InferType({ir_ts_type_a, ir_ts_type_b});
+    auto ir_mat_type_c =
+        ir_packed_matrix_multiply_op_creator->InferType({ir_mat_type_a, ir_mat_type_b});
 
-    auto ir_operator_type = ir::OperatorType::Create({ir_ts_type_a, ir_ts_type_b}, ir_ts_type_c);
+    auto ir_operator_type = ir::OperatorType::Create({ir_mat_type_a, ir_mat_type_b}, ir_mat_type_c);
     auto ir_operator = ir_builder->CreateOperatorByCreator(ir_packed_matrix_multiply_op_creator,
-                                                           {ir_ts_type_a, ir_ts_type_b});
+                                                           {ir_mat_type_a, ir_mat_type_b});
     auto gemm_optimizer = optimization::GemmOptimizer::Create();
     auto ir_gemm_operator = gemm_optimizer->Optimize(ir_operator);
 
@@ -117,12 +87,12 @@ TEST(GaloisTests, TestGemm) {
     auto mat_mul_fun =
         jit_engine->EmitOperatorSymbol<float *(*)(float *, float *)>(ir_gemm_operator);
 
-    auto shape_a = ir_ts_type_a->NormalizeShape();
-    auto shape_b = ir_ts_type_b->NormalizeShape();
+    auto shape_a = ir_mat_type_a->NormalizeShape();
+    auto shape_b = ir_mat_type_b->NormalizeShape();
 
     Eigen::MatrixRXf32 eigen_matrix_f32_a = Eigen::MatrixRXf32::Ones(shape_a[0], shape_a[1]);
     Eigen::MatrixRXf32 eigen_matrix_f32_b = Eigen::MatrixRXf32::Ones(shape_b[0], shape_b[1]);
-    auto shape_c = ir_ts_type_c->shape;
+    auto shape_c = ir_mat_type_c->shape;
 
     auto t0_eigen = std::chrono::high_resolution_clock::now();
     Eigen::MatrixRXf32 eigen_matrix_f32_expect = (eigen_matrix_f32_a * eigen_matrix_f32_b).eval();
