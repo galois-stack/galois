b7eb7ebf63e23319ece976dc29cdb174ddd2ae17
Zhang Zhimin <p3.1415@qq.com>
2025-03-22 21:10:54 +0800

    Add MatrixMultiplyWithTilePolicyTest

--- galois/ir/global_context.cpp
+++ galois/ir/global_context.cpp
@@ -12,6 +12,12 @@ GlobalContext global_context = GlobalContext(64);
 std::shared_ptr<TensorType> f32(FloatType::Create(32));
 std::shared_ptr<TensorType> f64(FloatType::Create(64));
 std::shared_ptr<TensorType> i8(IntType::Create(8, true));
+std::shared_ptr<TensorType> i16(IntType::Create(16, true));
+std::shared_ptr<TensorType> i32(IntType::Create(32, true));
 std::shared_ptr<TensorType> i64(IntType::Create(64, true));
+std::shared_ptr<TensorType> u8(IntType::Create(8, false));
+std::shared_ptr<TensorType> u16(IntType::Create(16, false));
+std::shared_ptr<TensorType> u32(IntType::Create(32, false));
+std::shared_ptr<TensorType> u64(IntType::Create(64, false));
 
 }  // namespace galois::ir
