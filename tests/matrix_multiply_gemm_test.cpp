396b7585680a6cceccc6608febffc27ed8a30320
Zhang Zhimin <p3.1415@qq.com>
2025-03-25 19:53:29 +0800

    save

--- tests/matrix_multiply_gemm_test.cpp
+++ tests/matrix_multiply_gemm_test.cpp
@@ -1,14 +1,15 @@
+#include <Eigen/Dense>
+#include <chrono>
 #include <cstdlib>
 #include <functional>
-#include <chrono>
-#include <Eigen/Dense>
 
 #include "galois/op/matrix_multiply.hpp"
 #include "galois/optimization/gemm_optimizer.hpp"
 #include "tests/galois_test.hpp"
 
-class MatrixMultiplyGemmTest : public testing::TestWithParam<
-                     std::tuple<std::shared_ptr<ir::TensorType>, int64_t, int64_t, int64_t>> {
+class MatrixMultiplyGemmTest
+    : public testing::TestWithParam<
+          std::tuple<std::shared_ptr<ir::TensorType>, int64_t, int64_t, int64_t>> {
    public:
     void SetUp() override {
         auto [ir_data_type, m, k, n] = GetParam();
@@ -18,7 +19,8 @@ class MatrixMultiplyGemmTest : public testing::TestWithParam<
 
         auto ir_builder = ir::Builder::Create();
         auto ir_packed_matrix_multiply_op_creator = op::MatrixMultiplyCreator::Create();
-        auto ir_mat_type_c = ir_packed_matrix_multiply_op_creator->InferType({ir_mat_type_a, ir_mat_type_b});
+        auto ir_mat_type_c =
+            ir_packed_matrix_multiply_op_creator->InferType({ir_mat_type_a, ir_mat_type_b});
 
         auto ir_operator = ir_builder->CreateOperatorByCreator(ir_packed_matrix_multiply_op_creator,
                                                                {ir_mat_type_a, ir_mat_type_b});
@@ -34,10 +36,12 @@ class MatrixMultiplyGemmTest : public testing::TestWithParam<
         auto normalize_n = ir_mat_type_b->NormalizeShape()[1];
         this->items = normalize_m * normalize_k * normalize_n;
 
-
-        this->sp_aligned256_mem_a = std::shared_ptr<void>( std::aligned_alloc(32, normalize_m * normalize_k * ir_data_type->bytes), [](void *p) { free(p); });
-        this->sp_aligned256_mem_b = std::shared_ptr<void>( std::aligned_alloc(32, normalize_k * normalize_n * ir_data_type->bytes), [](void *p) { free(p); });
-
+        this->sp_aligned256_mem_a = std::shared_ptr<void>(
+            std::aligned_alloc(32, normalize_m * normalize_k * ir_data_type->bytes),
+            [](void *p) { free(p); });
+        this->sp_aligned256_mem_b = std::shared_ptr<void>(
+            std::aligned_alloc(32, normalize_k * normalize_n * ir_data_type->bytes),
+            [](void *p) { free(p); });
     }
 
     void TearDown() override {
@@ -65,12 +69,10 @@ TEST_P(MatrixMultiplyGemmTest, TestMatrixMultiplyGemm) {
     free(mat_ptr_c);
 }
 
-
-
-
- // ir::i16不支持需要修复,
+// ir::i16不支持需要修复,
 INSTANTIATE_TEST_SUITE_P(MatrixMultiplyGemmTest, MatrixMultiplyGemmTest,
-    testing::Combine(testing::Values(ir::i8),  //  f32
-                     testing::Values(64, 512, 1024),              // m
-                     testing::Values(64, 512, 1024),              // n
-                     testing::Values(64, 512, 2014)));            // k
+                         testing::Combine(testing::Values(ir::f64, ir::f32, ir::i32,
+                                                          ir::i8),       //  f32
+                                          testing::Values(512, 1024),    // m
+                                          testing::Values(512, 1024),    // n
+                                          testing::Values(512, 2014)));  // k
