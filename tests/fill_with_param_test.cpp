efce4d3a921ed054973469312fccd7758682532e
Zhang Zhimin <p3.1415@qq.com>
2025-03-27 22:20:54 +0800

    Fix aligned_alloc error

--- tests/fill_with_param_test.cpp
+++ tests/fill_with_param_test.cpp
@@ -3,6 +3,7 @@
 #include <cstdlib>
 #include <functional>
 #include <memory>
+
 #include "tests/galois_test.hpp"
 #include "tests/test_utils.h"
 
@@ -13,7 +14,7 @@ class FillWithParamTest
     void SetUp() override {
         auto [ir_data_type, shape, fill_value] = GetParam();
         this->ir_data_type = ir_data_type;
-        this->shape = shape;  
+        this->shape = shape;
         this->fill_value = fill_value;
 
         Eigen::VectorXi64 eigen_shape = Eigen::Map<Eigen::VectorXi64>(shape.data(), shape.size());
@@ -26,75 +27,63 @@ class FillWithParamTest
             ir_builder->CreateOperatorByCreator(ir_fill_creator, {ir_input_type, ir_data_type});
 
         this->jit_engine = jit::Engine::Create();
-        fill_fun = jit_engine->EmitOperatorSymbol<void *(*)(void *, void *)>(ir_operator);
+        this->fill_fun = jit_engine->EmitOperatorSymbol<void (*)(void *, void *)>(ir_operator);
 
-        auto normalize_m = ir_input_type->NormalizeShape()[0];
-        auto normalize_k = ir_input_type->NormalizeShape()[1];
-        this->items = normalize_m * normalize_k;
-        this->sp_aligned256_mem_a = std::shared_ptr<void>(
-            std::aligned_alloc(32, this->items * ir_data_type->bytes),
-            [](void *p) { free(p); });
+        this->items = ir_input_type->Size();
+        this->input_vec.resize(this->items * ir_data_type->bytes);
     }
 
     void TearDown() override {
-        
         double galois_cost_time_seconds = galois_cost_time / 1e9;
         double bandwidth = items / (galois_cost_time_seconds * 1024 * 1024 * 1024);
 
-        fmt::print("Galois cost time: {}ns, galois flops: {:.04f}gops, bandwidth: {:.04f}\n", galois_cost_time,
-                   items / galois_cost_time, bandwidth);
+        fmt::print("Galois cost time: {}ns, galois flops: {:.04f}gops, bandwidth: {:.04f}\n",
+                   galois_cost_time, items / galois_cost_time, bandwidth);
     }
 
-    std::function<void *(void *, void *)> fill_fun;
+    std::function<void(void *, void *)> fill_fun;
     std::shared_ptr<galois::jit::Engine> jit_engine;
     std::shared_ptr<void> sp_aligned256_mem_a;
     std::vector<int64_t> shape;  // 多维形状
-    std::shared_ptr<ir::TensorType> ir_data_type;  
+    std::shared_ptr<ir::TensorType> ir_data_type;
+    std::vector<int8_t> input_vec;
 
     double items;
     double galois_cost_time;
     float fill_value;
-    
 };
 
 TEST_P(FillWithParamTest, TestFillWithParamTest) {
-
     // 执行 fill 算子
     auto t0 = std::chrono::high_resolution_clock::now();
-    auto fill_ptr_c = fill_fun(this->sp_aligned256_mem_a.get(), &this->fill_value);
+    fill_fun(this->input_vec.data(), &this->fill_value);
     auto t1 = std::chrono::high_resolution_clock::now();
     this->galois_cost_time = static_cast<double>((t1 - t0).count());
 
-    // 验证
-    void *raw_ptr = sp_aligned256_mem_a.get();
-
     if (auto float_type = std::dynamic_pointer_cast<ir::FloatType>(this->ir_data_type)) {
         if (float_type->bits == 32) {
-            float *float_ptr = static_cast<float *>(raw_ptr);
+            float *float_ptr = reinterpret_cast<float *>(input_vec.data());
             float float_fill_value = static_cast<float>(this->fill_value);  // 类型转换
             for (int i = 0; i < this->items; i++) {
                 GALOIS_ASSERT(float_ptr[i] == float_fill_value);
-            }
-        } else if (float_type->bits == 64) {
-            double *double_ptr = static_cast<double *>(raw_ptr);
-            double double_fill_value = static_cast<double>(this->fill_value);  // 类型转换
-            for (int i = 0; i < this->items; i++) {
-                GALOIS_ASSERT(double_ptr[i] == double_fill_value);
             }
         }
     }
 }
 
-
+INSTANTIATE_TEST_SUITE_P(Scalar, FillWithParamTest,
+                         testing::Combine(testing::Values(ir::f32),  // 数据类型
+                                          testing::Values(std::vector<int64_t>{1},
+                                                          std::vector<int64_t>{1, 1},
+                                                          std::vector<int64_t>{1, 1, 1},
+                                                          std::vector<int64_t>{1, 1, 1, 1}),
+                                          testing::Values(1.0f, 9.0f, 42.0f)),  // fill_value
+                         galois::test::PrintTestNameWithVector);
 
 INSTANTIATE_TEST_SUITE_P(FillWithParamTest, FillWithParamTest,
-                            testing::Combine(
-                                            testing::Values(ir::f32),  // 数据类型
-                                            testing::Values(
-                                                std::vector<int64_t>{512, 512},           // 2D
-                                                std::vector<int64_t>{512, 512, 3},        // 3D
-                                                std::vector<int64_t>{512, 512, 3, 4}      // 4D
-                                            ),  
-                                            testing::Values(1.0f, 9.0f, 42.0f)),          // fill_value
-                                            galois::test::PrintTestNameWithVector
-                            );
+                         testing::Combine(testing::Values(ir::f32),  // 数据类型
+                                          testing::Values(std::vector<int64_t>{512, 512},
+                                                          std::vector<int64_t>{512, 512, 3},
+                                                          std::vector<int64_t>{512, 512, 3, 4}),
+                                          testing::Values(1.0f, 9.0f, 42.0f)),  // fill_value
+                         galois::test::PrintTestNameWithVector);
