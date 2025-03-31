9cb6eaf7eca3925cb6372789eef52c5c3dac5f16
Zhang Zhimin <p3.1415@qq.com>
2025-03-31 17:15:47 +0800

    Refactor gemm test (#30)

--- tests/gemm_test.cpp
+++ tests/gemm_test.cpp
@@ -2,68 +2,6 @@
 #include "galois/optimization/gemm_optimizer.hpp"
 #include "tests/galois_test.hpp"
 
-TEST(GaloisTests, TestPackedMatrixMultiply) {
-    auto native_cpu_info = optimization::NativeCpuInfo::Create();
-    auto mat_mul_tile_policy = optimization::MatrixMultiplyTilePolicy::Create();
-    auto [ir_mat_type_a, ir_mat_type_b, simd_mat_mul_kernel] = mat_mul_tile_policy->Tile(ir::f32, native_cpu_info);
-    // 太小测不出性能， 所以又Tile了一次
-    ir_mat_type_a = ir_mat_type_a->Tile(64, 64);
-    ir_mat_type_b = ir_mat_type_b->Tile(64, 64);
-
-    auto ir_builder = ir::Builder::Create();
-    ir_builder->matrix_multiply_kernel_queue.push_back(simd_mat_mul_kernel);
-    auto ir_packed_matrix_multiply_op_creator = op::MatrixMultiplyCreator::Create();
-    auto ir_operator = ir_builder->CreateOperatorByCreator(ir_packed_matrix_multiply_op_creator,
-                                                           {ir_mat_type_a, ir_mat_type_b});
-
-    auto jit_engine = jit::Engine::Create();
-    auto mat_mul_fun = jit_engine->EmitOperatorSymbol<float *(*)(float *, float *)>(ir_operator);
-
-    auto shape_a = ir_mat_type_a->NormalizeShape();
-    auto shape_b = ir_mat_type_b->NormalizeShape();
-
-    Eigen::MatrixRXf32 eigen_matrix_f32_a = Eigen::MatrixRXf32::Ones(shape_a[0], shape_a[1]);
-    Eigen::MatrixRXf32 eigen_matrix_f32_b = Eigen::MatrixRXf32::Ones(shape_b[0], shape_b[1]);
-
-    auto t0 = std::chrono::high_resolution_clock::now();
-    auto ir_mat_c_ptr = mat_mul_fun(eigen_matrix_f32_a.data(), eigen_matrix_f32_b.data());
-    auto t1 = std::chrono::high_resolution_clock::now();
-    fmt::print("cost time: {}ns, galois flops: {:.04f}gops\n", (t1 - t0).count(),
-               shape_a[0] * shape_a[1] * shape_b[1] * 2 / static_cast<double>((t1 - t0).count()));
-    free(ir_mat_c_ptr);
-}
-
-TEST(GaloisTests, TestPackedMatrixMultiply_i8x16x1x16) {
-    //  一种快捷写法, 需要用TensorTypePointer包装后才支持这种写法
-    auto ir_ts_type_a = ir::i8->Tile(16, 1)->Tile(1, 2048)->Tile(128, 1);
-    auto ir_ts_type_b = ir::i8->Tile(1, 16)->Tile(2048, 1)->Tile(1, 128);
-    ir_ts_type_a->value_type->enable_multi_thread = true;
-
-    auto ir_builder = ir::Builder::Create();
-    ir_builder->matrix_multiply_kernel_queue.push_back(
-        op::SimdMatrixMultiplyKernel::Create(128));
-    auto ir_packed_matrix_multiply_op_creator = op::MatrixMultiplyCreator::Create();
-    auto ir_operator = ir_builder->CreateOperatorByCreator(ir_packed_matrix_multiply_op_creator,
-                                                           {ir_ts_type_a, ir_ts_type_b});
-
-    auto jit_engine = jit::Engine::Create();
-    auto mat_mul_fun = jit_engine->EmitOperatorSymbol<int8_t *(*)(int8_t *, int8_t *)>(ir_operator);
-
-    auto shape_a = ir_ts_type_a->NormalizeShape();
-    auto shape_b = ir_ts_type_b->NormalizeShape();
-
-    Eigen::MatrixRXi8 eigen_matrix_i8_a = Eigen::MatrixRXi8::Zero(shape_a[0], shape_a[1]);
-    Eigen::MatrixRXi8 eigen_matrix_i8_b = Eigen::MatrixRXi8::Zero(shape_b[0], shape_b[1]);
-
-    auto t0 = std::chrono::high_resolution_clock::now();
-    auto ir_mat_c_ptr = mat_mul_fun(eigen_matrix_i8_a.data(), eigen_matrix_i8_b.data());
-    auto t1 = std::chrono::high_resolution_clock::now();
-    fmt::print("cost time: {}ns, galois flops: {:.04f}gops\n", (t1 - t0).count(),
-               shape_a[0] * shape_a[1] * shape_b[1] * 2 / static_cast<double>((t1 - t0).count()));
-
-    free(ir_mat_c_ptr);
-}
-
 TEST(GaloisTests, TestGemm) {
     //  一种快捷写法, 需要用TensorTypePointer包装后才支持这种写法
     auto ir_mat_type_a = f32->Tile(1536, 1024);
@@ -102,7 +40,7 @@ TEST(GaloisTests, TestGemm) {
     auto f32_c_ptr = mat_mul_fun(eigen_matrix_f32_a.data(), eigen_matrix_f32_b.data());
     auto t1 = std::chrono::high_resolution_clock::now();
 
-    fmt::print("cost time: {}ns, galois flops: {:.04f}gops\n", (t1 - t0).count(),
+    fmt::print("cost time: {}ns, galois glops: {:.04f}gops\n", (t1 - t0).count(),
                shape_a[0] * shape_a[1] * shape_b[1] * 2 / static_cast<double>((t1 - t0).count()));
 
     auto get_f32_c = [=](int64_t i, int64_t j) -> float { return f32_c_ptr[i * shape_c[1] + j]; };
@@ -126,3 +64,85 @@ TEST(GaloisTests, TestGemm) {
 
     free(static_cast<void *>(f32_c_ptr));
 }
+
+class GemmPerformanceTest
+    : public testing::TestWithParam<
+          std::tuple<std::shared_ptr<ir::TensorType>, int64_t, int64_t, int64_t>> {
+   public:
+    void SetUp() override {
+        auto [ir_data_type, m, k, n] = GetParam();
+
+        auto ir_mat_type_a = ir_data_type->Tile(m, k);
+        auto ir_mat_type_b = ir_data_type->Tile(k, n);
+
+        auto ir_builder = ir::Builder::Create();
+        auto ir_packed_matrix_multiply_op_creator = op::MatrixMultiplyCreator::Create();
+        auto ir_mat_type_c =
+            ir_packed_matrix_multiply_op_creator->InferType({ir_mat_type_a, ir_mat_type_b});
+
+        auto ir_operator = ir_builder->CreateOperatorByCreator(ir_packed_matrix_multiply_op_creator,
+                                                               {ir_mat_type_a, ir_mat_type_b});
+
+        auto gemm_optimizer = optimization::GemmOptimizer::Create();
+        auto ir_gemm_operator = gemm_optimizer->Optimize(ir_operator);
+
+        this->jit_engine = jit::Engine::Create();
+        mat_mul_fun = jit_engine->EmitOperatorSymbol<void *(*)(void *, void *)>(ir_gemm_operator);
+
+        auto normalize_m = ir_mat_type_a->NormalizeShape()[0];
+        auto normalize_k = ir_mat_type_a->NormalizeShape()[1];
+        auto normalize_n = ir_mat_type_b->NormalizeShape()[1];
+        this->items = normalize_m * normalize_k * normalize_n;
+
+        this->sp_aligned256_mem_a = std::shared_ptr<void>(
+            std::aligned_alloc(32, normalize_m * normalize_k * ir_data_type->bytes),
+            [](void *p) { free(p); });
+        this->sp_aligned256_mem_b = std::shared_ptr<void>(
+            std::aligned_alloc(32, normalize_k * normalize_n * ir_data_type->bytes),
+            [](void *p) { free(p); });
+    }
+
+    void TearDown() override {
+        fmt::print("Galois cost time: {}ns, galois glops: {:.04f}gops\n", galois_cost_time,
+                   items * 2 / galois_cost_time);
+    }
+
+    std::function<void *(void *, void *)> mat_mul_fun;
+    std::shared_ptr<galois::jit::Engine> jit_engine;
+
+    std::shared_ptr<void> sp_aligned256_mem_a;
+    std::shared_ptr<void> sp_aligned256_mem_b;
+
+    double items;
+    double galois_cost_time;
+};
+
+TEST_P(GemmPerformanceTest, TestMatrixMultiplyGemm) {
+    // 执行 Galois 矩阵乘法
+    auto t0 = std::chrono::high_resolution_clock::now();
+    auto mat_ptr_c = mat_mul_fun(this->sp_aligned256_mem_a.get(), this->sp_aligned256_mem_b.get());
+    auto t1 = std::chrono::high_resolution_clock::now();
+    this->galois_cost_time = static_cast<double>((t1 - t0).count());
+    // 释放内存
+    free(mat_ptr_c);
+}
+
+// TODO: has bug when i8
+// INSTANTIATE_TEST_SUITE_P(Large, GemmPerformanceTest,
+//                          testing::Combine(testing::Values(ir::f64, ir::f32, ir::i32,
+//                                                           ir::i8),      //  f32
+//                                           testing::Values(500, 1000),   // m
+//                                           testing::Values(500, 1000),   // n
+//                                           testing::Values(500, 1000)),  // k
+//                          galois::test::PrintTestName                    // 自定义测试名称
+// );
+
+// ir::i16不支持需要修复,
+INSTANTIATE_TEST_SUITE_P(Large2, GemmPerformanceTest,
+                         testing::Combine(testing::Values(ir::f64, ir::f32, ir::i32,
+                                                          ir::i8),      //  f32
+                                          testing::Values(512, 1024),   // m
+                                          testing::Values(512, 1024),   // n
+                                          testing::Values(512, 1024)),  // k
+                         galois::test::PrintTestName                    // 自定义测试名称
+);
