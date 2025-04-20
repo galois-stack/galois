8d3ef7cfbd7abb8461b8e3f3be5b383a83beab0e
Zhang Zhimin <p3.1415@qq.com>
2025-04-21 03:04:23 +0800

    Complete ArithmeticInstruction

--- galois/op/arithmetic.hpp
+++ galois/op/arithmetic.hpp
@@ -25,13 +25,19 @@ class ArithmeticCreator : public BinaryCreator {
                            std::shared_ptr<ir::Tensor> ir_input1,
                            std::shared_ptr<ir::Tensor> ir_output,
                            std::shared_ptr<ir::Builder> ir_builder) override {
+        if (ir_input0->type->IsScalar() && ir_input1->type->IsScalar()) {
+            auto ir_re = ir_builder->Create<ir::ArithmeticInstruction>(Operation, ir_input0,
+                                                                       ir_input1);
+            ir_builder->Create<ir::Write>(ir_re, ir_output);
+            return;
+        }
+
         auto [ir_grid, scope_guard] = ir_builder->CreateGrid(ir_output->type->shape);
         auto ir_accessor_out = ir_builder->CreateIdentityAccessor(ir_output);
         auto ir_accessor_in0 = ir_builder->CreateIdentityAccessor(ir_input0);
         auto ir_accessor_in1 = ir_builder->CreateIdentityAccessor(ir_input1);
-        auto ir_add = ir_builder->Create<ir::ArithmeticInstruction>(Operation, ir_accessor_in0,
-                                                                    ir_accessor_in1);
-        ir_builder->Create<ir::Write>(ir_add, ir_accessor_out);
+
+        this->AffineExpressImpl(ir_accessor_in0, ir_accessor_in1, ir_accessor_out, ir_builder);
     }
 };
 
