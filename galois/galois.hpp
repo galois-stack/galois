8c6c01a8a4024ec7c74c1a33386dbafcd68a19d6
Zhang Zhimin <p3.1415@qq.com>
2025-04-21 23:12:18 +0800

    Add matrix multiply micro kernel test

--- galois/galois.hpp
+++ galois/galois.hpp
@@ -3,6 +3,7 @@
 #include "galois/codegen/cpu/prajna_codegen.hpp"
 #include "galois/framework/framework.hpp"
 #include "galois/ir/ir.hpp"
+#include "galois/ir/operation_count_visitor.hpp"
 #include "galois/ir/visitor.hpp"
 #include "galois/jit/engine.hpp"
 #include "galois/op/op.hpp"
--- tests/operation_count_test.cpp
+++ tests/operation_count_test.cpp
@@ -1,7 +1,7 @@
 
+#include "galois/ir/operation_count_visitor.hpp"
 #include "galois/op/matrix_multiply.hpp"
 #include "galois/optimization/gemm_optimizer.hpp"
-#include "galois/ir/operation_count_visitor.hpp"
 #include "tests/galois_test.hpp"
 
 TEST(GaloisTests, TestOperationCount) {
@@ -20,7 +20,7 @@ TEST(GaloisTests, TestOperationCount) {
     auto normalize_n = ir_mat_type_b->NormalizeShape()[1];
     auto items = normalize_m * normalize_k * normalize_n * 2;
 
-    auto operation_count_visitor = galois::transform::OperationCounter::Create();
+    auto operation_count_visitor = ir::OperationCounter::Create();
     auto operation_count = operation_count_visitor->CountOperation(ir_operator);
     ASSERT_EQ(items, operation_count);
 }
