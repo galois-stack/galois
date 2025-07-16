65e5bbe5699b579cf6a4a63998a0022145902e83
WanRui37 <1446145652@qq.com>
2025-07-16 19:31:01 +0800

    add pybind_example

--- third_party/pybind_example/include/pybind_example.h
+++ third_party/pybind_example/include/pybind_example.h
@@ -1,11 +1,7 @@
-// error_calculation.h
-#pragma once
-#include <memory>
 #include <pybind11/pybind11.h>
 #include <pybind11/embed.h>
 #include <pybind11/numpy.h>
-
-namespace py = pybind11;
+#include <memory>
 
 // 定义一个函数来计算误差
 void calculate_error(void* mem_a, void* mem_b, void* mem_c, int normalize_m, int normalize_k, int normalize_n);
---
+++ third_party/pybind_example/src/error_calculation.cpp
@@ -0,0 +1,28 @@
+// error_calculation.cpp
+#include "pybind_example.h"
+#include <fmt/core.h>
+
+void calculate_error(void* mem_a, void* mem_b, void* mem_c, int normalize_m, int normalize_k, int normalize_n) {
+    pybind11::scoped_interpreter guard{};
+
+    // 使用pybind11thon的Numpybind11和pybind11Torch进行误差计算
+    pybind11::module_ torch = pybind11::module_::import("torch");
+    
+    // 创建pybind11Torch张量
+    pybind11::array_t<float> arr_a({normalize_m, normalize_k}, static_cast<float*>(mem_a));
+    pybind11::array_t<float> arr_b({normalize_k, normalize_n}, static_cast<float*>(mem_b));
+    pybind11::array_t<float> arr_c_custom({normalize_m, normalize_n}, static_cast<float*>(mem_c));
+    
+    // 转换为pybind11Torch张量
+    pybind11::object tensor_a = torch.attr("from_numpybind11")(arr_a);
+    pybind11::object tensor_b = torch.attr("from_numpybind11")(arr_b);
+    pybind11::object tensor_c_custom = torch.attr("from_numpybind11")(arr_c_custom);
+    
+    // 执行矩阵乘法
+    pybind11::object tensor_c = tensor_a.attr("matmul")(tensor_b);
+    
+    // 计算误差指标
+    pybind11::object abs_error = tensor_c.attr("sub")(tensor_c_custom).attr("abs")();
+    float max_error = abs_error.attr("max")().attr("item")().cast<float>();
+    float mean_error = abs_error.attr("mean")().attr("item")().cast<float>();
+}
